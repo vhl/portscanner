3ce236b7fcc61ce0b273ba5f5edef9bf1d0d92ff
Oliver Day <oday@vistahigherlearning.com>
2019-08-23 11:50:03 -0400

    Add open port verification.

--- portscanner.rb
+++ portscanner.rb
@@ -1,6 +1,7 @@
 require 'socket'
 require 'json'
 require 'slack-notifier'
+require 'net/https'
 
 PORTBOT_WEBHOOK = ENV["PORTBOT_WEBHOOK"]
 PORTBOT_TARGET = ENV["PORTBOT_TARGET"]
@@ -9,7 +10,7 @@ notifier = Slack::Notifier.new PORTBOT_WEBHOOK
 
 TIMEOUT = 2
 thread_count = 48
-ports_open = 0
+ports_open = []
 
 q = Queue.new
 
@@ -26,6 +27,22 @@ PORT_LIST.shuffle!
 # populate the queue
 PORT_LIST.each do |port|
   q.push(port)
+end
+
+def verify_open_port(port)
+  # This is the HTTP request we send to fetch a file
+  request = "GET / HTTP/1.0\r\n\r\n"
+  begin
+    socket = TCPSocket.open(PORTBOT_TARGET, port)  # Connect to server
+    socket.print(request)               # Send request
+    response = socket.read              # Read complete response
+    # Split response at first blank line into headers and body
+    headers,body = response.split("\r\n\r\n", 2) 
+  rescue Errno::ECONNREFUSED => e
+    return false
+  end
+  return true
+
 end
 
 # create threads
@@ -46,8 +63,9 @@ threads   = thread_count.times.map do |i|
       end
       _, sockets, _ = IO.select(nil, [socket], nil, TIMEOUT)
       if sockets
-        ports_open += 1
-        notifier.ping "<!channel> UNAUTHORIZED PORT OPEN ON MAESTRO: #{port}"
+        verified = verify_open_port(port)
+        ports_open.push(port) if verified
+        notifier.ping "<!channel> UNAUTHORIZED PORT OPEN ON MAESTRO: #{port}" if verified
       end
     end
   end
@@ -56,4 +74,4 @@ end
 q.close
 threads.each(&:join)
 
-notifier.ping "<!channel> TOTAL UNAUTHORIZED PORTS OPEN ON MAESTRO: #{ports_open}" if ports_open > 0
+notifier.ping "<!channel> TOTAL UNAUTHORIZED PORTS OPEN ON MAESTRO: #{ports_open.size}" if ports_open.size > 0
